608d8ec1b68cf9b089fcf798c66da065846977b7
Komil Tokhirov <komil.tokhirov@gmail.com>
2024-09-13 01:52:30 +0900

    added default formatting to clangformat

--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -1,22 +1,23 @@
 local function get_default_config_path(config_file)
-    -- Get the full path of the current Lua script
-    local info = debug.getinfo(1, "S")
+	-- Get the full path of the current Lua script
+	local info = debug.getinfo(1, "S")
 
-    -- Removes the "@" character from the path
-    local script_path = info.source:sub(2)
+	-- Removes the "@" character from the path
+	local script_path = info.source:sub(2)
 
-    -- Get the directory of the script
-    local format_directory = vim.fn.fnamemodify(script_path, ":h")
+	-- Get the directory of the script
+	local format_directory = vim.fn.fnamemodify(script_path, ":h")
 
-    return vim.fs.joinpath(format_directory, "formatter_configs", config_file)
+	return vim.fs.joinpath(format_directory, "formatter_configs", config_file)
 end
 
 local function get_command_for_config(config_file, default_command, project_command)
-    if vim.fn.findfile(config_file, ".;") ~= "" then
-        return project_command or { exe = default_command.exe }
-    end
+	if vim.fn.findfile(config_file, ".;") ~= "" then
+		return project_command or { exe = default_command.exe }
+	end
 
-    return default_command or { exe = project_command.exe }
+	vim.notify("Couldn't find format config in worktree, using default config")
+	return default_command or { exe = project_command.exe }
 end
 
 return {
@@ -30,11 +31,40 @@ return {
 		})
 	end,
 	config = function()
+		local util = require("formatter.util")
 		require("formatter").setup({
 			filetype = {
 				lua = { require("formatter.filetypes.lua").stylua },
-				cpp = { require("formatter.filetypes.cpp").clangformat },
-				c = { require("formatter.filetypes.c").clangformat },
+				cpp = {
+					function()
+						local config_file = ".clang-format"
+						local exe = "clang-format"
+						return get_command_for_config(config_file, {
+							exe = exe,
+							args = {
+								"-style=file:" .. get_default_config_path(config_file),
+								"-assume-filename",
+								util.escape_path(util.get_current_buffer_file_name()),
+							},
+							stdin = true,
+						}, require("formatter.filetypes.cpp").clangformat())
+					end,
+				},
+				c = {
+					function()
+						local config_file = ".clang-format"
+						local exe = "clang-format"
+						return get_command_for_config(config_file, {
+							exe = exe,
+							args = {
+								"-style=file:" .. get_default_config_path(config_file),
+								"-assume-filename",
+								util.escape_path(util.get_current_buffer_file_name()),
+							},
+							stdin = true,
+						}, require("formatter.filetypes.c").clangformat())
+					end,
+				},
 				rust = { require("formatter.filetypes.rust").rustfmt },
 				cs = { require("formatter.filetypes.cs").csharpier },
 				python = { require("formatter.filetypes.python").ruff },
@@ -45,16 +75,13 @@ return {
 					end,
 				},
 				scala = {
-                    function()
-                        local config_file = ".scalafmt.conf"
-                        return get_command_for_config(
-                            config_file,
-                            {
-                                exe = "scalafmt",
-                                args = { "--config", get_default_config_path(config_file) }
-                            }
-                        )
-                    end,
+					function()
+						local config_file = ".scalafmt.conf"
+						return get_command_for_config(config_file, {
+							exe = "scalafmt",
+							args = { "--config", get_default_config_path(config_file) },
+						})
+					end,
 				},
 			},
 		})
