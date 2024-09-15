797fe9a39598c040177b9497da5bc258e9a7371e
Komil Tokhirov <komil.tokhirov@gmail.com>
2024-09-16 03:15:05 +0900

    fixed powershell local.ps1

--- powershell/config/set.ps1
+++ powershell/config/set.ps1
@@ -8,6 +8,9 @@ $env:XDG_CONFIG_HOME = "$HOME\.config"
 $env:XDG_DATA_HOME = "$HOME\.local\share"
 $env:XDG_STATE_HOME = "$HOME\.local\share"
 
+# yazi
+$env:YAZI_CONFIG_HOME = "$env:XDG_CONFIG_HOME\yazi"
+
 # nvim stuff
 $nvim = "$env:XDG_CONFIG_HOME\nvim"
 $nvimdata = "$env:XDG_STATE_HOME\nvim-data"
