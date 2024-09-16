ba57d128c0c825b8a4c5d85011fe2cc32a466761
Komil Tokhirov <komil.tokhirov@gmail.com>
2024-09-16 16:39:04 +0900

    added q, n, g aliases

--- powershell/config/set.ps1
+++ powershell/config/set.ps1
@@ -1,5 +1,22 @@
 Set-PSReadLineOption -PredictionSource None
 
+# Yazi setup
+function y {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
+
+function q {
+    Exit
+}
+
+Set-Alias n nvim
+Set-Alias g git
 Set-Alias sha Get-FileHash
 Remove-Item alias:wget
 
