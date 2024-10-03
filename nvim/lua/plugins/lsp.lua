de371b09245b1876b9c40cdcc27eea7c5ace23aa
Komil Tokhirov <komil.tokhirov@gmail.com>
2024-10-03 13:07:10 +0900

    nvim: changed lsp bindings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,15 +2,15 @@ local set_lsp_maps = function(opts)
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 	vim.keymap.set("n", "<leader>vks", vim.lsp.buf.workspace_symbol, opts)
-	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "<leader>ve", vim.diagnostic.open_float, opts)
 	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("v", "<leader>vca", function()
+	vim.keymap.set("n", "<leader>va", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("v", "<leader>va", function()
 		vim.lsp.buf.range_code_action()
 	end, opts)
-	vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-	vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+	vim.keymap.set("n", "<leader>vd", vim.lsp.buf.references, opts)
+	vim.keymap.set("n", "<leader>vr", vim.lsp.buf.rename, opts)
 	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end
 
@@ -58,6 +58,7 @@ local setup_lspconfigs = function()
 		"pylsp",
 		"ruff",
 		"clangd",
+		"metals",
 	}
 	for _, lsp in ipairs(lsps) do
 		lspconfig[lsp].setup({})
