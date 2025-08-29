a36777056e0b724f439b702abbc08ed7be5b1bbc
Komil Tokhirov <komil.tokhirov@gmail.com>
2025-08-29 22:50:17 +0500

    wezterm: added gradient background and disabled kitty keyboard

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,18 +4,28 @@ local config = {}
 config.default_prog = { "powershell", "-NoLogo" }
 
 config.font = wezterm.font("JetBrains Mono")
+config.font_size = 13
 config.color_scheme = "Tokyo Night Storm"
 
-config.enable_kitty_keyboard = true
+-- config.enable_kitty_keyboard = true
 
 config.window_decorations = "RESIZE"
 config.use_fancy_tab_bar = false
+
 config.tab_max_width = 32
 config.window_padding = {
-	left = "1cell",
-	right = "1cell",
-	top = "0cell",
+	left = "0.5cell",
+	right = "0.5cell",
+	top = "0.4cell",
 	bottom = "0cell",
+}
+
+-- config.color_scheme = "Github"
+configs = {}
+config.window_background_gradient = {
+	-- colors = { "#001f4d", "#49024b" },
+	colors = { "#152e48", "#242a49" },
+	orientation = "Horizontal",
 }
 
 -- start maximized
