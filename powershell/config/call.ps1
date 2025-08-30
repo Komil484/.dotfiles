a82cf449bd2a9cdd5d09c96e64e039dcb0348df5
Komil Tokhirov <komil.tokhirov@gmail.com>
2025-08-30 15:23:22 +0500

    powershell: added fzf and zoxide configs

--- powershell/config/call.ps1
+++ powershell/config/call.ps1
@@ -1,2 +1,6 @@
 # Oh-my-posh setup
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json" | Invoke-Expression
+
+# Zoxide setup
+# Must be at the end
+Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
--- powershell/config/set.ps1
+++ powershell/config/set.ps1
@@ -39,3 +39,39 @@ $env:YAZI_CONFIG_HOME = "$env:XDG_CONFIG_HOME\yazi"
 # nvim stuff
 $nvim = "$env:XDG_CONFIG_HOME\nvim"
 $nvimdata = "$env:XDG_STATE_HOME\nvim-data"
+
+# Fzf
+$env:FZF_DEFAULT_OPTS="
+    --highlight-line
+    --info=inline
+    --ansi
+    --layout=reverse
+    --border=rounded
+    --keep-right
+    --height=45%
+    --cycle
+
+    --color=bg+:#2e3c64
+    --color=bg:-1
+    --color=border:#29a4bd
+    --color=fg:#c0caf5
+    --color=gutter:-1
+    --color=header:#ff9e64
+    --color=hl+:#2ac3de
+    --color=hl:#2ac3de
+    --color=info:#545c7e
+    --color=marker:#ff007c
+    --color=pointer:#ff007c
+    --color=prompt:#2ac3de
+    --color=query:#c0caf5:regular
+    --color=scrollbar:#29a4bd
+    --color=separator:#ff9e64
+    --color=spinner:#ff007c
+"
+
+# Zoxide
+$env:_ZO_FZF_OPTS="$env:FZF_DEFAULT_OPTS
+   --no-sort
+   --bind=ctrl-z:ignore
+   --exit-0
+"
